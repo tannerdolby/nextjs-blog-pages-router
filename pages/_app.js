474f64b67ac3ab78a3c7a0508eb31377e21ddc1a
Tanner <tannercdolby@gmail.com>
2024-08-22 23:48:19 -0700

    Wrap app in ThemeContext Provider to support theme switching and render theme switch btn

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,38 @@
 import '../styles/global.css';
+import { createContext, useState, useEffect } from 'react';
 
-export default function App({ Component, pageProps}) {
-    return <Component {...pageProps} />
+export const ThemeContext = createContext(null);
+
+export default function App({ Component, pageProps }) {
+    const [theme, setTheme] = useState('light');
+
+    useEffect(() => {
+        const html = document.querySelector('html');
+        if (theme === 'dark') {
+            html.classList.add('dark');
+            document.body.classList.add('dark');
+        } else {
+            html.classList.remove('dark');
+            document.body.classList.remove('dark');
+        }
+    }, [theme]);
+
+    return (
+        <ThemeContext.Provider value={theme}>
+            <Component {...pageProps} />
+            <button
+                className='theme-switcher-btn'
+                title="Toggle theme"
+                onClick={() => {
+                    if (theme === 'light') {
+                        setTheme('dark');
+                    } else {
+                        setTheme('light');
+                    }
+                }}
+            >
+                {theme === 'light' ? '🌙' : '☀️'}
+            </button>
+        </ThemeContext.Provider>
+    );
 }
